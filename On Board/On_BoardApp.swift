5bacc2828f59c96fda1ea2741873c113fd4092aa
jawadkhadra <jawad.alkhadra@gmail.com>
2026-06-05 13:26:50 -0700

    Initial Commit

---
+++ On Board/On_BoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  On_BoardApp.swift
+//  On Board
+//
+//  Created by Jawad Khadra on 6/5/26.
+//
+
+import SwiftUI
+
+@main
+struct On_BoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ On BoardTests/On_BoardTests.swift
@@ -0,0 +1,19 @@
+//
+//  On_BoardTests.swift
+//  On BoardTests
+//
+//  Created by Jawad Khadra on 6/5/26.
+//
+
+import Testing
+@testable import On_Board
+
+struct On_BoardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
